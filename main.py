eefa8414592487bd04b5ef08e83535493b9d4839
Robotsrule4545 <caleb.mincraft18@gmail.com>
2023-02-22 11:54:52 +0800

    add functions

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
+#!/usr/bin/env python
 import pygame
 from pygame.locals import *
+import towers.blaster
+import os
+import sys
+# import numpy
+
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+
+
+def load_image(file):
+    file = os.path.join(main_dir, "assets", file)
+    surface = pygame.image.load(file)
+    return surface.convert()
+
+
+def exit(bool):
+    if bool:
+        pygame.quit()
+        sys.exit()
 
 
 def main():
-    # Initialise screen
+    # Init
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+
+    # Dividable by 80, 10x8 grid of 16x16 squares.
+    screen = pygame.display.set_mode((800, 640))
     pygame.display.set_caption('Robot\'s Tower Defense')
+    icon = pygame.transform.scale(load_image("icon.png"), (128, 128))
+    pygame.display.set_icon(icon)
 
     background = pygame.Surface(screen.get_size())
     background = background.convert()
     background.fill((0, 0, 0))
-    pygame.display.flip()
 
-    # Event loop
+    # Update loop
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
-                return
-
+                exit(True)
+        # Add update code here.
         screen.blit(background, (0, 0))
+        # Might be a problem when the background gets pasted over sprites
         pygame.display.flip()
 
 
-if __name__ == '__main__': main()
+if __name__ == '__main__':
+    main()
---
+++ towers/blaster.py
@@ -0,0 +1,6 @@
+import pygame as pg
+
+
+class blaster(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
