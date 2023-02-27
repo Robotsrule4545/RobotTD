2f978f229e7891b64a5f97570c3cc871870ba107
Robotsrule4545 <caleb.mincraft18@gmail.com>
2023-02-27 17:05:22 +0800

    Polish and get some help.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import pygame
 from pygame.locals import *
-import towers.blaster
 import os
 import sys
+import random
+
+screen_height = 400
+screen_width = 320
 # import numpy
 
 
@@ -22,11 +25,28 @@ def exit(bool):
         sys.exit()
 
 
+class Blaster(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((20, 20))
+        self.image.fill((255, 255, 255))
+        pos = pygame.mouse.get_pos()
+        self.rect = self.image.get_rect(center=pos)
+        self.rect = self.image.get_rect(center=(screen_height / 2, screen_width / 2))
+        self.rect = self.image.get_rect(center=(random.randint(0, screen_height), random.randint(0, screen_width)))
+
+    def update(self):
+        # placeholder
+        pos = pygame.mouse.get_pos()
+        if abs(pos[0] - self.rect.x) < 20 and abs(pos[1] - self.rect.y) < 20 and pygame.mouse.get_pressed()[0]:
+            self.kill()
+
+
 def main():
     # Init
     pygame.init()
-
-    # Dividable by 80, 10x8 grid of 16x16 squares.
+    last_click = 0
+    # Divisible by 80, 10x8 grid of 16x16 squares.
     screen = pygame.display.set_mode((800, 640))
     pygame.display.set_caption('Robot\'s Tower Defense')
     icon = pygame.transform.scale(load_image("icon.png"), (128, 128))
@@ -34,15 +54,27 @@ def main():
 
     background = pygame.Surface(screen.get_size())
     background = background.convert()
-    background.fill((0, 0, 0))
+
+    blaster_group = pygame.sprite.Group()
 
     # Update loop
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 exit(True)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                blaster = Blaster()
+                blaster_group.add(blaster)
+                last_click = pygame.time.get_ticks()
+        if pygame.time.get_ticks() > 10000 + last_click:
+            exit(True)
+
         # Add update code here.
+
         screen.blit(background, (0, 0))
+
+        blaster_group.draw(screen)
+        blaster_group.update()
         # Might be a problem when the background gets pasted over sprites
         pygame.display.flip()
 
--- towers/blaster.py
+++
@@ -1,6 +0,0 @@
-import pygame as pg
-
-
-class blaster(pg.sprite.Sprite):
-    def __init__(self):
-        pg.sprite.Sprite.__init__(self)
