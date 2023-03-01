a38db5ae407c9f96d01311339bb3de38ee44d569
Robotsrule4545 <caleb.mincraft18@gmail.com>
2023-03-01 11:03:48 +0800

    Adding functionality.

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from pygame.locals import *
 import os
 import sys
 import random
-
 screen_height = 400
 screen_width = 320
-# import numpy
-
+placing_ready = False
+pausedGame = False
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 
@@ -19,21 +18,23 @@ def load_image(file):
     return surface.convert()
 
 
-def exit(bool):
-    if bool:
+def exit(e):
+    if e:
         pygame.quit()
+        print("Quitting")
         sys.exit()
 
 
 class Blaster(pygame.sprite.Sprite):
-    def __init__(self):
+    images = []
+
+    def __init__(self, pos):
         super().__init__()
-        self.image = pygame.Surface((20, 20))
+        self.image = pygame.Surface((80, 80))
         self.image.fill((255, 255, 255))
         pos = pygame.mouse.get_pos()
         self.rect = self.image.get_rect(center=pos)
-        self.rect = self.image.get_rect(center=(screen_height / 2, screen_width / 2))
-        self.rect = self.image.get_rect(center=(random.randint(0, screen_height), random.randint(0, screen_width)))
+        self.rect = self.image.get_rect(center=(pos[0], pos[1]))
 
     def update(self):
         # placeholder
@@ -45,12 +46,11 @@ class Blaster(pygame.sprite.Sprite):
 def main():
     # Init
     pygame.init()
-    last_click = 0
     # Divisible by 80, 10x8 grid of 16x16 squares.
-    screen = pygame.display.set_mode((800, 640))
     pygame.display.set_caption('Robot\'s Tower Defense')
     icon = pygame.transform.scale(load_image("icon.png"), (128, 128))
     pygame.display.set_icon(icon)
+    screen = pygame.display.set_mode((800, 640))
 
     background = pygame.Surface(screen.get_size())
     background = background.convert()
@@ -59,22 +59,19 @@ def main():
 
     # Update loop
     while True:
+        global currentMousePos
+        currentMousePos = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == QUIT:
                 exit(True)
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                blaster = Blaster()
+            if event.type == pygame.MOUSEBUTTONDOWN: # and placing_ready == True:
+                blaster = Blaster(currentMousePos)
                 blaster_group.add(blaster)
-                last_click = pygame.time.get_ticks()
-        if pygame.time.get_ticks() > 10000 + last_click:
-            exit(True)
-
-        # Add update code here.
-
         screen.blit(background, (0, 0))
+        pos = pygame.mouse.get_pos()
 
         blaster_group.draw(screen)
-        blaster_group.update()
+        blaster_group.update(currentMousePos)
         # Might be a problem when the background gets pasted over sprites
         pygame.display.flip()
 
